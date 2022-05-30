4b527162938f18976fc6a5eddc51bbc777d37146
Nevermore毓 <260602448@qq.com>
2022-05-30 16:34:45 +0800

    新增 单元测试、actuator项目监控

---
+++ src/main/java/com/xmy/demonowcoder/actuator/DatabaseEndpoint.java
@@ -0,0 +1,45 @@
+package com.xmy.demonowcoder.actuator;
+
+import com.xmy.demonowcoder.util.CommunityUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * 自定义项目监控类
+ *
+ * @author xumingyu
+ * @date 2022/5/30
+ **/
+@Component
+@Endpoint(id = "database")
+public class DatabaseEndpoint {
+
+    private static final Logger logger = LoggerFactory.getLogger(DatabaseEndpoint.class);
+
+    @Autowired
+    private DataSource dataSource;
+
+    // 相当于GET请求
+    @ReadOperation
+    public String checkConnection() {
+        try (
+                // 放到try这个位置就不用释放资源，底层自动释放
+                Connection conn = dataSource.getConnection();
+        ) {
+            return CommunityUtil.getJSONString(0, "获取连接成功!");
+        } catch (SQLException e) {
+            logger.error("获取连接失败:" + e.getMessage());
+            return CommunityUtil.getJSONString(1, "获取连接失败!");
+        }
+    }
+
+
+}
